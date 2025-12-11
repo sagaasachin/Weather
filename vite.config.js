db50efa6f7dcd907308efede66c5e4dae8c5a7af
sagaasachin <sagaasachin48@gmail.com>
2025-12-11 18:02:55 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/weather/", // <-- IMPORTANT for GitHub Pages
+  base: "/Weather/", // <-- IMPORTANT for GitHub Pages
   plugins: [
     react({
       babel: {
